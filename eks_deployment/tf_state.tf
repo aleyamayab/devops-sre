a0916315436f76215ba19c219ac783bd7130a6c7
aleyamayab <kingdom_ale@hotmail.com>
2024-03-08 21:28:41 -0600

    add

--- eks_deployment/tf_state.tf
+++ eks_deployment/tf_state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-curse"
-    key            = "EC2/terraform.tfstate"
+    key            = "pt/terraform.tfstate"
     region         = "us-east-1"  # Ajusta a tu región
     encrypt        = false
     profile        = "alejo-sandbox"  # Nombre de tu perfil AWS configurado en la CLI
