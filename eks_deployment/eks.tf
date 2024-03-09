3732954db485d595b2d4a6578df1b91bcd9af44c
Alejandro Amaya <100615826+aleyamayab@users.noreply.github.com>
2024-03-09 03:03:55 -0600

    Update eks.tf

--- eks_deployment/eks.tf
+++ eks_deployment/eks.tf
@@ -3,17 +3,14 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
 
-  cluster_name    = "my-eks-cluster-deploy" # Replace with your desired cluster name
+  cluster_name    = "cluster-deploy" # Replace with your desired cluster name
   cluster_version = "1.24"                  # Replace with your desired EKS version
-
   vpc_id                          = "vpc-0a8418569c7846653" # Replace with your VPC ID
-  subnet_ids                      = ["subnet-082693184ac10b951", "subnet-06e3835b9f61e0751", "subnet-0c480d98fe8d2e6e8"]
+  subnet_ids                      = ["subnet-082693184ac10b951", "subnet-06e3835b9f61e0751", "subnet-04526f45cd9228bd9"]
   cluster_endpoint_private_access = true
   cluster_endpoint_public_access  = true
 
-  manage_aws_auth_configmap = true #Manejar usuario por el modulo de terraform
-
-  eks_managed_node_groups = {
+   eks_managed_node_groups = {
     eks_nodes = {
       desired_capacity = 1
       max_capacity     = 2
@@ -26,6 +23,3 @@ module "eks" {
     }
   }
 }
-
-
-
