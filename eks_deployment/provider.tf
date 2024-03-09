1686090a84a87d15ec78d4def83a5493f8b3ac62
Alejandro Amaya <100615826+aleyamayab@users.noreply.github.com>
2024-03-09 03:04:12 -0600

    Update provider.tf

--- eks_deployment/provider.tf
+++ eks_deployment/provider.tf
@@ -3,5 +3,7 @@
 # -------------------------
 provider "aws" {
   region  = "us-east-1"     # Especifica tu región AWS
+  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files = ["~/.aws/config"]
   profile = "alejo-sandbox" # Especifica el nombre de tu perfil AWS configurado en la CLI
 }
